66b053f683b9624a6874feb82f76152cb0a312c3
nirzaf <mfmfazrin1986@gmail.com>
2022-05-11 18:54:50 +0530

    Add project files.

---
+++ sample_3_3/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sample_3_3 {
+  public partial class Form1 : DevExpress.XtraEditors.XtraForm {
+    public Form1() {
+      InitializeComponent();
+    }
+
+    private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e) {
+      var child = new ChildForm();
+      child.MdiParent = this;
+      child.Show();
+    }
+  }
+}
